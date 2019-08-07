8704b3df9933b687c798896b4d4160d8378fedd0
Diffblue Assistant <42613982+diffblue-assistant@users.noreply.github.com>
2019-08-07 08:47:37 +0000

    Add Unit Tests for the following classes:
    com.alibaba.csp.sentinel.config.SentinelConfigTest
    com.alibaba.csp.sentinel.log.LogBaseTest
    com.alibaba.csp.sentinel.node.metric.MetricNodeTest
    com.alibaba.csp.sentinel.node.metric.MetricWriterTest
    com.alibaba.csp.sentinel.slots.block.flow.ClusterFlowConfigTest
    com.alibaba.csp.sentinel.slots.block.flow.FlowRuleComparatorTest
    com.alibaba.csp.sentinel.slots.block.flow.FlowRuleTest
    com.alibaba.csp.sentinel.slots.block.flow.FlowRuleUtilTest
    com.alibaba.csp.sentinel.slots.block.flow.controller.DefaultControllerTest
    com.alibaba.csp.sentinel.util.StringUtilTest
    
    These tests were written using Diffblue Cover

---
+++ sentinel-core/src/test/java/com/alibaba/csp/sentinel/node/metric/MetricNodeTest.java
@@ -0,0 +1,198 @@
+package com.alibaba.csp.sentinel.node.metric;
+
+import com.alibaba.csp.sentinel.node.metric.MetricNode;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.Timeout;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MetricNodeTest {
+
+  @Rule public final ExpectedException thrown = ExpectedException.none();
+
+  @Rule public final Timeout globalTimeout = new Timeout(10000);
+
+  /* testedClasses: MetricNode */
+  // Test written by Diffblue Cover.
+  @Test
+  public void fromFatStringInputNullOutputNullPointerException() {
+
+    // Arrange
+    final String line = null;
+
+    // Act
+    thrown.expect(NullPointerException.class);
+    MetricNode.fromFatString(line);
+
+    // Method is not expected to return due to exception thrown
+  }
+
+  // Test written by Diffblue Cover.
+  @Test
+  public void fromThinStringInputNullOutputNullPointerException() {
+
+    // Arrange
+    final String line = null;
+
+    // Act
+    thrown.expect(NullPointerException.class);
+    MetricNode.fromThinString(line);
+
+    // Method is not expected to return due to exception thrown
+  }
+
+  // Test written by Diffblue Cover.
+  @Test
+  public void getBlockQpsOutputZero() {
+
+    // Arrange
+    final MetricNode objectUnderTest = new MetricNode();
+
+    // Act
+    final long actual = objectUnderTest.getBlockQps();
+
+    // Assert result
+    Assert.assertEquals(0L, actual);
+  }
+
+  // Test written by Diffblue Cover.
+  @Test
+  public void getExceptionQpsOutputZero() {
+
+    // Arrange
+    final MetricNode objectUnderTest = new MetricNode();
+
+    // Act
+    final long actual = objectUnderTest.getExceptionQps();
+
+    // Assert result
+    Assert.assertEquals(0L, actual);
+  }
+
+  // Test written by Diffblue Cover.
+  @Test
+  public void getOccupiedPassQpsOutputZero() {
+
+    // Arrange
+    final MetricNode objectUnderTest = new MetricNode();
+
+    // Act
+    final long actual = objectUnderTest.getOccupiedPassQps();
+
+    // Assert result
+    Assert.assertEquals(0L, actual);
+  }
+
+  // Test written by Diffblue Cover.
+  @Test
+  public void getPassQpsOutputZero() {
+
+    // Arrange
+    final MetricNode objectUnderTest = new MetricNode();
+
+    // Act
+    final long actual = objectUnderTest.getPassQps();
+
+    // Assert result
+    Assert.assertEquals(0L, actual);
+  }
+
+  // Test written by Diffblue Cover.
+  @Test
+  public void getResourceOutputNull() {
+
+    // Arrange
+    final MetricNode objectUnderTest = new MetricNode();
+
+    // Act
+    final String actual = objectUnderTest.getResource();
+
+    // Assert result
+    Assert.assertNull(actual);
+  }
+
+  // Test written by Diffblue Cover.
+  @Test
+  public void getRtOutputZero() {
+
+    // Arrange
+    final MetricNode objectUnderTest = new MetricNode();
+
+    // Act
+    final long actual = objectUnderTest.getRt();
+
+    // Assert result
+    Assert.assertEquals(0L, actual);
+  }
+
+  // Test written by Diffblue Cover.
+  @Test
+  public void getSuccessQpsOutputZero() {
+
+    // Arrange
+    final MetricNode objectUnderTest = new MetricNode();
+
+    // Act
+    final long actual = objectUnderTest.getSuccessQps();
+
+    // Assert result
+    Assert.assertEquals(0L, actual);
+  }
+
+  // Test written by Diffblue Cover.
+  @Test
+  public void getTimestampOutputZero() {
+
+    // Arrange
+    final MetricNode objectUnderTest = new MetricNode();
+
+    // Act
+    final long actual = objectUnderTest.getTimestamp();
+
+    // Assert result
+    Assert.assertEquals(0L, actual);
+  }
+
+  // Test written by Diffblue Cover.
+  @Test
+  public void setResourceInputNotNullOutputVoid() {
+
+    // Arrange
+    final MetricNode objectUnderTest = new MetricNode();
+    final String resource = "3";
+
+    // Act
+    objectUnderTest.setResource(resource);
+
+    // Assert side effects
+    Assert.assertEquals("3", objectUnderTest.getResource());
+  }
+
+  // Test written by Diffblue Cover.
+  @Test
+  public void toStringOutputNotNull() throws InvocationTargetException {
+
+    // Arrange
+    final MetricNode objectUnderTest = new MetricNode();
+    objectUnderTest.setRt(1L);
+    objectUnderTest.setTimestamp(3L);
+    objectUnderTest.setBlockQps(1L);
+    objectUnderTest.setPassQps(786_438L);
+    objectUnderTest.setExceptionQps(69_893_478L);
+    objectUnderTest.setOccupiedPassQps(5L);
+    objectUnderTest.setResource("1a 2b 3c");
+    objectUnderTest.setSuccessQps(8L);
+
+    // Act
+    final String actual = objectUnderTest.toString();
+
+    // Assert result
+    Assert.assertEquals(
+        "MetricNode{timestamp=3, passQps=786438, blockQps=1, successQps=8, exceptionQps=69893478, rt=1, occupiedPassQps=5, resource=\'1a 2b 3c\'}",
+        actual);
+  }
+}
